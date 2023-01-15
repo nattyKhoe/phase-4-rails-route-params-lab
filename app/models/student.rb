e753d4a5a44794ed03930f7968a570286a3fe096
Natasha <natasha.khoe@gmail.com>
2023-01-15 11:40:13 +0800

    commit tree 9f67b1ce3423a270d111bd24ae738baddd2104e2

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ApplicationRecord
-
+  #class methor written using a scope
+  scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
   def to_s
     "#{first_name} #{last_name}"
   end
